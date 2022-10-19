d0adab69ebfda200eba908a5cd6c169d5bbc5bcc
Javad Moradi <47134413+tahaafg20@users.noreply.github.com>
2022-10-19 13:52:21 +0800

    Changed loader size

--- lib/src/widgets/indicator.dart
+++ lib/src/widgets/indicator.dart
@@ -69,7 +69,7 @@ class _LoadingIndicatorState extends State<LoadingIndicator> {
         );
         break;
       case EasyLoadingIndicatorType.riaLoader:
-        _indicator = Image.asset("assets/ria_loader.gif", height:90, width:90);
+        _indicator = Image.asset("assets/ria_loader.gif", height:80, width:80);
         break;
       case EasyLoadingIndicatorType.threeBounce:
         _indicator = SpinKitThreeBounce(
